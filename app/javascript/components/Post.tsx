ee4a8ce3cad13acf129b0ced1d324e143f0852dd
Julian Vargas <julianvargasalvarez@gmail.com>
2018-03-01 21:59:55 -0500

    type script working like a charm

---
+++ app/javascript/components/Post.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+
+class Post extends React.Component<any, any> {
+  render () {
+    return (
+    <div>
+     Hola mundo
+    </div>
+    );
+  }
+}
+
+export default Post
